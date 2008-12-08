fd374a55c42477437d3e0d7391334f611ab0b2bd
Michal Špaček <skim@cpan.org>
2008-12-08 14:25:42 +0000

    Rewrite to better version (perlcritic).

--- Structure/Output/Raw.pm
+++ Structure/Output/Raw.pm
@@ -4,23 +4,29 @@ package CSS::Structure::Output::Raw;
 
 # Pragmas.
 use strict;
+use warnings;
 
 # Modules.
 use Error::Simple::Multiple;
+use List::MoreUtils qw(none);
+use Readonly;
+
+# Constants.
+Readonly::Scalar my $EMPTY => q{};
 
 # Version.
 our $VERSION = 0.01;
 
 #------------------------------------------------------------------------------
-sub new($@) {
+sub new {
 #------------------------------------------------------------------------------
 # Constructor.
 
-	my $class = shift;
+	my ($class, @params) = @_;
 	my $self = bless {}, $class;
 
 	# Set output handler.
-	$self->{'output_handler'} = '';
+	$self->{'output_handler'} = $EMPTY;
 
 	# Skip bad tags.
 	$self->{'skip_bad_tags'} = 0;
@@ -29,20 +35,20 @@ sub new($@) {
 	$self->{'comment_delimeters'} = ['/*', '*/'];
 
 	# Process params.
-	while (@_) {
-		my $key = shift;
-		my $val = shift;
-		err "Bad parameter '$key'." unless exists $self->{$key};
+	while (@params) {
+		my $key = shift @params;
+		my $val = shift @params;
+		err "Bad parameter '$key'." if ! exists $self->{$key};
 		$self->{$key} = $val;
 	}
 
 	# Check to comment delimeters.
-	if ((! grep { $_ eq $self->{'comment_delimeters'}->[0] } 
+	if ((none { $_ eq $self->{'comment_delimeters'}->[0] }
 		('/*', '<!--'))
-		|| (! grep { $_ eq $self->{'comment_delimeters'}->[1] }
+		|| (none { $_ eq $self->{'comment_delimeters'}->[1] }
 		('*/', '-->'))) {
 		
-		err "Bad comment delimeters.";
+		err 'Bad comment delimeters.';
 	}
 
 	# Reset.
@@ -53,55 +59,58 @@ sub new($@) {
 }
 
 #------------------------------------------------------------------------------
-sub flush($) {
+sub flush {
 #------------------------------------------------------------------------------
 # Flush css structure in object.
 
 	my $self = shift;
 	my $ouf = $self->{'output_handler'};
 	if ($ouf) {
-		print $ouf $self->{'flush_code'};
+		print {$ouf} $self->{'flush_code'};
+		return;
 	} else {
 		return $self->{'flush_code'};
 	}
 }
 
 #------------------------------------------------------------------------------
-sub put($@) {
+sub put {
 #------------------------------------------------------------------------------
 # Put css structure code.
 
-	my $self = shift;
-	my @data = @_;
+	my ($self, @data) = @_;
 
 	# For every data.
 	foreach my $dat (@data) {
 
 		# Bad data.
-		unless (ref $dat eq 'ARRAY') {
-			err "Bad data.";
+		if (ref $dat ne 'ARRAY') {
+			err 'Bad data.';
 		}
 
 		# Detect and process data.
 		$self->_detect_data($dat);
 	}
+	return;
 }
 
 #------------------------------------------------------------------------------
-sub reset($) {
+sub reset {
 #------------------------------------------------------------------------------
 # Resets internal variables.
 
 	my $self = shift;
 
 	# Flush code.
-	$self->{'flush_code'} = '';
+	$self->{'flush_code'} = $EMPTY;
 
 	# Tmp code.
 	$self->{'tmp_code'} = [];
 
 	# Open selector flag.
 	$self->{'open_selector'} = 0;
+
+	return;
 }
 
 #------------------------------------------------------------------------------
@@ -109,7 +118,7 @@ sub reset($) {
 #------------------------------------------------------------------------------
 
 #------------------------------------------------------------------------------
-sub _detect_data($$) {
+sub _detect_data {
 #------------------------------------------------------------------------------
 # Detect and process data.
 
@@ -117,11 +126,11 @@ sub _detect_data($$) {
 
 	# Definitions.
 	if ($data->[0] eq 'd') {
-		if ($#{$self->{'tmp_code'}} > -1) {
+		if (scalar @{$self->{'tmp_code'}}) {
 			$self->_flush_tmp;
 		}
-		unless ($self->{'open_selector'}) {
-			err "No selector.";
+		if (! $self->{'open_selector'}) {
+			err 'No selector.';
 		}
 		shift @{$data};
 		while (@{$data}) {
@@ -145,17 +154,17 @@ sub _detect_data($$) {
 		shift @{$data};
 		$self->{'flush_code'} .= $self->{'comment_delimeters'}->[0];
 		foreach my $d (@{$data}) {
-			$self->{'flush_code'} .= ref $d eq 'SCALAR' ? ${$d} 
+			$self->{'flush_code'} .= ref $d eq 'SCALAR' ? ${$d}
 				: $d;
 		}
 		$self->{'flush_code'} .= $self->{'comment_delimeters'}->[1];
 
 	# End of selector.
 	} elsif ($data->[0] eq 'e') {
-		unless ($self->{'open_selector'}) {
-			err "Bad ending of selector.";
+		if (! $self->{'open_selector'}) {
+			err 'Bad ending of selector.';
 		}
-		$self->{'flush_code'} .= "}";
+		$self->{'flush_code'} .= '}';
 		$self->{'open_selector'} = 0;
 
 	# Raw data.
@@ -168,23 +177,29 @@ sub _detect_data($$) {
 
 	# Other.
 	} else {
-		err "Bad type of data." if $self->{'skip_bad_tags'};
+		err 'Bad type of data.' if $self->{'skip_bad_tags'};
 	}
+	return;
 }
 
 #------------------------------------------------------------------------------
-sub _flush_tmp($) {
+sub _flush_tmp {
 #------------------------------------------------------------------------------
 # Flush $self->{'tmp_code'}.
 
 	my $self = shift;
-	$self->{'flush_code'} .= join(',', @{$self->{'tmp_code'}})."{";
+	$self->{'flush_code'} .= join(',', @{$self->{'tmp_code'}}).'{';
 	$self->{'tmp_code'} = [];
+	return;
 }
 
 1;
 
+__END__
+
 =pod
+
+=encoding utf8
 
 =head1 NAME
 
@@ -244,18 +259,24 @@ sub _flush_tmp($) {
 
 =back
 
-=head1 REQUIREMENTS
+=head1 DEPENDENCIES
 
-L<Error::Simple::Multiple>
+L<Error::Simple::Multiple(3pm)>,
+L<List::MoreUtils(3pm)>,
+L<Readonly(3pm)>.
 
 =head1 SEE ALSO
 
-L<CSS::Structure>, 
-L<CSS::Structure::Output::Indent>
+L<CSS::Structure(3pm)>,
+L<CSS::Structure::Output::Indent(3pm)>.
 
 =head1 AUTHOR
 
- Michal Spacek L<tupinek@gmail.com>
+ Michal Špaček L<tupinek@gmail.com>
+
+=head1 LICENSE AND COPYRIGHT
+
+ BSD license.
 
 =head1 VERSION
 
